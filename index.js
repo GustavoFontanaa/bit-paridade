2c8db32bba528fc36ea0b6e3ff9186d958f81c02
Gustavo Fontana <gustavoasf77@gmail.com>
2023-06-21 23:11:59 -0300

    comments

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-const CRC_POLYNOMIAL_16 = 0x8005; // CRC-16 polynomial
-const INITIAL_CRC_VALUE_16 = 0xffff; // Initial CRC value for CRC-16
-const CRC_MASK_16 = 0xffff; // Mask the CRC value to 16 bits
+const CRC_POLYNOMIAL_16 = 0x8005; // Polinômio CRC-16
+const INITIAL_CRC_VALUE_16 = 0xffff; // Valor inicial do CRC para CRC-16
+const CRC_MASK_16 = 0xffff; // Máscara para manter o valor do CRC em 16 bits
 
-const CRC_POLYNOMIAL_32 = 0x04c11db7; // CRC-32 polynomial
-const INITIAL_CRC_VALUE_32 = 0xffffffff; // Initial CRC value for CRC-32
-const CRC_MASK_32 = 0xffffffff; // Mask the CRC value to 32 bits
+const CRC_POLYNOMIAL_32 = 0x04c11db7; // Polinômio CRC-32
+const INITIAL_CRC_VALUE_32 = 0xffffffff; // Valor inicial do CRC para CRC-32
+const CRC_MASK_32 = 0xffffffff; // Máscara para manter o valor do CRC em 32 bits
 
+// Função para calcular o CRC-16
 function generateCRC16(inputData) {
   let crc = INITIAL_CRC_VALUE_16;
 
   for (let i = 0; i < inputData.length; i++) {
-    crc ^= (inputData.charCodeAt(i) << 8) & CRC_MASK_16; // XOR with next byte
+    crc ^= (inputData.charCodeAt(i) << 8) & CRC_MASK_16; // XOR com o próximo byte
 
     for (let j = 0; j < 8; j++) {
       if ((crc & 0x8000) !== 0) {
-        crc = (crc << 1) ^ CRC_POLYNOMIAL_16;
+        crc = (crc << 1) ^ CRC_POLYNOMIAL_16; // Aplica o polinômio ao CRC
       } else {
         crc = crc << 1;
       }
     }
   }
 
-  crc = crc & CRC_MASK_16;
-  return crc.toString(16).toUpperCase();
+  crc = crc & CRC_MASK_16; // Aplica a máscara para manter o CRC em 16 bits
+  return crc.toString(16).toUpperCase(); // Converte o CRC para uma string hexadecimal maiúscula
 }
 
+// Função para calcular o CRC-32
 function generateCRC32(inputData) {
   let crc = INITIAL_CRC_VALUE_32;
 
   for (let i = 0; i < inputData.length; i++) {
-    crc ^= (inputData.charCodeAt(i) << 24) & CRC_MASK_32; // XOR with next byte
+    crc ^= (inputData.charCodeAt(i) << 24) & CRC_MASK_32; // XOR com o próximo byte
 
     for (let j = 0; j < 8; j++) {
       if ((crc & 0x80000000) !== 0) {
-        crc = (crc << 1) ^ CRC_POLYNOMIAL_32;
+        crc = (crc << 1) ^ CRC_POLYNOMIAL_32; // Aplica o polinômio ao CRC
       } else {
         crc = crc << 1;
       }
     }
   }
 
-  crc = crc & CRC_MASK_32;
-  return crc.toString(16).toUpperCase();
+  crc = crc & CRC_MASK_32; // Aplica a máscara para manter o CRC em 32 bits
+  return crc.toString(16).toUpperCase(); // Converte o CRC para uma string hexadecimal maiúscula
 }
 
+// Função para verificar se o CRC-16 recebido é igual ao CRC calculado
 function verifyCRC16(inputData, receivedCRC) {
   const calculatedCRC = generateCRC16(inputData);
   return calculatedCRC === receivedCRC;
 }
 
+// Função para verificar se o CRC-32 recebido é igual ao CRC calculado
 function verifyCRC32(inputData, receivedCRC) {
   const calculatedCRC = generateCRC32(inputData);
   return calculatedCRC === receivedCRC;
 }
 
+// Código principal para receber a entrada do usuário e verificar os CRCs
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -95,6 +100,7 @@ readline.question('Digite uma palavra: ', (inputData) => {
   });
 });
 
+// Função para verificar se uma string contém apenas caracteres ASCII
 function isASCII(str) {
   return /^[\x00-\x7F]*$/.test(str);
 }
